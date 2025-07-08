07da07896031aedd9835350dbbf5000f614dfc36
Frekidus Ketemaw <85076411+Frekidus@users.noreply.github.com>
2025-07-08 21:44:12 +0300

    Create create_vector_store.py

---
+++ src/create_vector_store.py
@@ -0,0 +1,7 @@
+from langchain_community.vectorstores import FAISS
+
+def create_vector_store(chunks, embedding_model, save_path="faiss_index"):
+    db = FAISS.from_texts(chunks, embedding_model)
+    db.save_local(save_path)
+    print(f"✅ Vector store saved to '{save_path}'")
+    return db
